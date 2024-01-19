9320f7846d8c3d678dfd2a1a8b925767d76343a1
Weiyi Lee <weiyilee@Weiyide-iMac-2.router00195f.com>
2024-01-19 17:08:19 +0800

    Split type checking to github from vercel

--- next.config.js
+++ next.config.js
@@ -24,6 +24,12 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;
