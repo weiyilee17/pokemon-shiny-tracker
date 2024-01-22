29ea0d12de63bf25d3a60ad2cfb4063932c6804e
Weiyi Lee <weiyilee@Weiyide-iMac-2.router00195f.com>
2024-01-22 15:10:48 +0800

    Add dark mode toggle button

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,24 @@
 import type { PropsWithChildren } from "react";
+import { ThemeProvider } from "./theme-provider";
 
 function PageLayout({ children }: PropsWithChildren) {
   return (
-    <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <div className="container flex flex-col gap-12 px-4 py-16 ">
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col items-center justify-center gap-4">
-            {children}
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <main className="flex min-h-screen flex-col">
+        <div className="container flex flex-col gap-12 px-4 pb-16 pt-4">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col items-center justify-center gap-4">
+              {children}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </ThemeProvider>
   );
 }
 
