34c2f711caae545c540b68519ac0874a050cf6a3
Weiyi Lee <weiyilee@Weiyide-iMac-2.local>
2024-04-24 17:01:57 +0800

    Add virtualization and update pnpm version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## What's this app about?
 
 This is an app used to keep track of progress of shiny pokemons captured in the switch game `Pokemon Let's Go Eevee` and `Pokemon Let's Go Pikachu`, since there is no intuitive way of doing so in game. The main purpose is to get more experience with the T3 Stack for real problems, and practice drizzle.
+
+pnpm has backward compatability issue for v8 -> v9. Namely, the lock file version from v6 -> v9. For pnpm versions upgraded to v9 and above, after pulling the latest code, reinstalling the dependencies would fix the issue of failling to start the app.
--- src/components/pokemon_card_grid.tsx
+++ src/components/pokemon_card_grid.tsx
@@ -1,15 +1,86 @@
 import type { RouterOutputs } from "~/utils/api";
+import { forwardRef } from "react";
+import { VirtuosoGrid } from "react-virtuoso";
+
 import PokemonCard from "./pokemon_card";
+
+import type { ComponentProps, PropsWithChildren } from "react";
 
 type TPokemonsWithCaptured = RouterOutputs["pokemon"]["getAll"];
 
+const gridComponents = {
+  List: forwardRef<HTMLDivElement, ComponentProps<"div">>(
+    ({ style, children, ...props }, ref) => (
+      <div
+        ref={ref}
+        {...props}
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          ...style,
+        }}
+      >
+        {children}
+      </div>
+    ),
+  ),
+  Item: ({ children, ...props }: PropsWithChildren) => (
+    <div
+      {...props}
+      style={{
+        margin: "0.5rem",
+        display: "flex",
+        flex: "none",
+        alignContent: "stretch",
+        boxSizing: "border-box",
+      }}
+    >
+      {children}
+    </div>
+  ),
+};
+
+const ItemWrapper = ({ children, ...props }: PropsWithChildren) => (
+  <div
+    {...props}
+    style={{
+      display: "flex",
+      flex: 1,
+      whiteSpace: "nowrap",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const getItemCountent = (
+  _: number,
+  singlePokemon: TPokemonsWithCaptured[number],
+) => (
+  <ItemWrapper>
+    <PokemonCard {...singlePokemon} />
+  </ItemWrapper>
+);
+
 function PokemonCardGrid({ pokemons }: { pokemons: TPokemonsWithCaptured }) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 xl:grid-cols-6">
-      {pokemons.map((singlePokemon) => (
-        <PokemonCard {...singlePokemon} key={singlePokemon.id} />
-      ))}
-    </div>
+    <>
+      <VirtuosoGrid
+        // TODO: virtualization tends to use absolute positioning, See if the class can be dynamic
+        className="!h-[667px] !w-[350px] sm:!w-[640px] md:!h-[900px] md:!w-[768px] xl:!w-[1280px]"
+        data={pokemons}
+        components={gridComponents}
+        itemContent={getItemCountent}
+      />
+      <style>{`html, body, #root { height: 100% }`}</style>
+    </>
+
+    // <div className="grid grid-cols-1 gap-4 md:grid-cols-3 xl:grid-cols-6">
+    //   {pokemons.map((singlePokemon) => (
+    //     <PokemonCard {...singlePokemon} key={singlePokemon.id} />
+    //   ))}
+    // </div>
   );
 }
 
